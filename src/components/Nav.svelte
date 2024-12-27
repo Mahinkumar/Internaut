b022f3f9542fa795d0d3bd54f86d244fd47954de
Mahinkumar <73975593+Mahinkumar@users.noreply.github.com>
2024-12-27 09:06:56 +0530

    Improve Nav Bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { Segment } from '@skeletonlabs/skeleton-svelte';
+    import { Search } from 'lucide-svelte';
 
     let Mode = $state('Tech');
     let name = 'Internaut';
@@ -8,17 +9,20 @@
 
 
 
-<nav class="h-24 w-screen flex items-center justify-center border-b-2 border-white/10">
-    <ul class="h-16 w-screen flex justify-evenly items-center">
-      <ul class="w-1/3 h-full btn-group preset-outlined-surface-200-800 flex-col mr-2 ml-2 md:flex-row flex justify-center items-center">
-        <button type="button" onclick={()=>{Mode="Tech"}} class="btn w-1/3 {Mode == "Tech" ? 'preset-filled' : 'hover:preset-tonal' }">Tech</button>
-        <button type="button" onclick={()=>{Mode="Space"}} class="btn w-1/3 {Mode == "Space" ? 'preset-filled' : 'hover:preset-tonal' }">Space</button>
-        <button type="button" onclick={()=>{Mode="Dev"}} class="btn w-1/3 {Mode == "Dev" ? 'preset-filled' : 'hover:preset-tonal' }">Dev</button>
+<nav class="h-24 flex items-center justify-center border-b-2 border-white/10">
+    <ul class="h-16 flex justify-evenly items-center w-[80%]">
+      <ul class="w-1/3 h-[80%] btn-group flex-col mr-2 ml-2 md:flex-row flex justify-start items-center">
+        <button type="button" onclick={()=>{Mode="Tech"}} class="btn w-1/4 {Mode == "Tech" ? 'preset-filled' : 'hover:preset-tonal' }">Tech</button>
+        <button type="button" onclick={()=>{Mode="Space"}} class="btn w-1/4 {Mode == "Space" ? 'preset-filled' : 'hover:preset-tonal' }">Space</button>
+        <button type="button" onclick={()=>{Mode="Dev"}} class="btn w-1/4 {Mode == "Dev" ? 'preset-filled' : 'hover:preset-tonal' }">Dev</button>
+        <button type="button" onclick={()=>{Mode="Home"}} class="btn w-1/4 {Mode == "Home" ? 'preset-filled' : 'hover:preset-tonal' }">Home</button>
       </ul>
       <li class="w-1/3 h-full flex items-center justify-center text-2xl">{name}</li>
-      <li class="w-1/3 h-full flex items-center justify-center space-x-4"
-      ><a type="button" href="https://github.com/mahinkumar/Internaut" class="btn preset-filled">Star Github for updates</a>
-      <a type="button" href="https://github.com/mahinkumar" class="btn preset-filled">Visit Github Profile</a>
+      <li class="w-1/3 h-full flex items-center justify-evenly space-x-4"
+      ><a type="button" href="https://github.com/mahinkumar/Internaut" class="btn">Archives</a>
+      <a type="button" href="https://github.com/mahinkumar" class="btn">Bookmarks</a>
+      <button type="button" class="btn"><Search/></button>
+      
       </li>
     </ul>
 </nav>
